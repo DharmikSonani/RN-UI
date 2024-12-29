66e3edb5a65eebebb479c64376849104754e5ac8
dharmiksonani <dharmiksonani84618461@gmail.com>
2024-12-30 00:01:08 +0530

    IOS Deeplinking Setup

--- ios/RNUI/AppDelegate.mm
+++ ios/RNUI/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <React/RCTLinkingManager.h> // For DeepLinks
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -27,5 +29,32 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
+// Add Below Code for DeepLinks
+
+- (BOOL)application:(UIApplication *)application
+openURL:(NSURL *)url
+options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+  return [RCTLinkingManager application:application
+    continueUserActivity:userActivity
+    restorationHandler:restorationHandler];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+  return [RCTLinkingManager application:application openURL:url
+    sourceApplication:sourceApplication annotation:annotation];
+}
+
+//DEEPLINKS TILL HERE
 
 @end
