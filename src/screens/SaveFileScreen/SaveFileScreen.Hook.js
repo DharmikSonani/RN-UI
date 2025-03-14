d5cc7b2fdf693249c0991cb2aa53917570de6675
dharmiksonani <dharmiksonani84618461@gmail.com>
2025-03-14 13:16:57 +0530

    14/03/2025

--- src/screens/SaveFileScreen/SaveFileScreen.Hook.js
+++ src/screens/SaveFileScreen/SaveFileScreen.Hook.js
@@ -8,6 +8,17 @@ const useScreenHooks = (props) => {
     // variables
     const navigation = props.navigation;
     const { requestFilePermission } = useFilePermissions();
+    const demoUrls = [
+        'https://imgv3.fotor.com/images/slider-image/A-clear-image-of-a-woman-wearing-red-sharpened-by-Fotors-image-sharpener.jpg',
+        'https://images7.alphacoders.com/137/1378048.png',
+        'https://images.wallpapersden.com/image/download/assassins-s-creed-shadows-4k-gaming_bmdtZWmUmZqaraWkpJRobWllrWdmbm4.jpg',
+        'https://4kwallpapers.com/images/wallpapers/your-name-shooting-3840x2160-14938.jpg',
+        'https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg',
+        'https://cdn.pixabay.com/video/2024/10/14/236316_large.mp4',
+        'https://cdn.pixabay.com/video/2024/08/16/226795_large.mp4',
+        'https://cdn.pixabay.com/video/2025/01/19/253436_large.mp4',
+        'https://cdn.pixabay.com/video/2024/10/06/234930_large.mp4',
+    ]
 
     // useStates
     const [url, setUrl] = useState('');
@@ -35,9 +46,11 @@ const useScreenHooks = (props) => {
 
     return {
         navigation,
-        onSavePress,
+        demoUrls,
 
         url, setUrl,
+
+        onSavePress,
     };
 }
 
--- src/screens/SaveFileScreen/SaveFileScreen.js
+++ src/screens/SaveFileScreen/SaveFileScreen.js
@@ -1,4 +1,4 @@
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import useScreenHooks from './SaveFileScreen.Hook'
 import { styles } from './styles'
@@ -6,27 +6,47 @@ import { styles } from './styles'
 const SaveFileScreen = (props) => {
 
     const {
+        demoUrls,
         url, setUrl,
         onSavePress
     } = useScreenHooks(props)
 
     return (
         <View style={styles.Container}>
-            <TextInput
-                value={url}
-                onChangeText={setUrl}
-                placeholder='Url with file extention'
-                style={styles.InputStyle}
-                placeholderTextColor={'rgba(255,255,255,0.6)'}
-                keyboardType='url'
-                numberOfLines={1}
+            <View style={styles.RowContainer}>
+                <TextInput
+                    value={url}
+                    onChangeText={setUrl}
+                    placeholder='Url with file extention'
+                    style={styles.InputStyle}
+                    keyboardType='url'
+                    numberOfLines={1}
+                />
+                <TouchableOpacity
+                    style={styles.Button}
+                    onPress={onSavePress}
+                >
+                    <Text style={styles.ButtonText}>Save</Text>
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={demoUrls}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) =>
+                    <TouchableOpacity
+                        style={styles.ItemStyle}
+                        onPress={() => { setUrl(item) }}
+                    >
+                        <Text>{item}</Text>
+                    </TouchableOpacity>
+                }
+                showsVerticalScrollIndicator={true}
+                style={{ maxHeight: '40%', width: '100%' }}
+                contentContainerStyle={{
+                    gap: 15,
+                    width: '100%',
+                }}
             />
-            <TouchableOpacity
-                style={styles.Button}
-                onPress={onSavePress}
-            >
-                <Text style={styles.ButtonText}>Save</Text>
-            </TouchableOpacity>
         </View>
     )
 }
